2ad7109cb3ccc54f6eb42512a2547a8028a07311
kursatsmsek <kursatsimsek@protonmail.ch>
2023-12-21 17:32:11 +0300

    project was created and database properties

---
+++ src/main/java/com/kursatdev/springbootredismysql/SpringBootRedisMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.kursatdev.springbootredismysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kursatdev/springbootredismysql/SpringBootRedisMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursatdev.springbootredismysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
